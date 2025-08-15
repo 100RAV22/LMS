630f7abb103f842eb94b61ab4e76f290d9be6fe4
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-15 22:36:46 +0530

    lms project client structure

---
+++ client/src/components/student/TestimonialsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TestimonialsSection() {
+  return (
+    <div>
+      <h1>Testimonials section</h1>
+    </div>
+  )
+}
+
+export default TestimonialsSection
---
+++ client/src/pages/educator/MyCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyCourses() {
+  return (
+    <div>
+      <h1>Edu My courses</h1>
+    </div>
+  )
+}
+
+export default MyCourses
