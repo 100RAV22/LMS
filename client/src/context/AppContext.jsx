a877cff6b1fa87c0c9265ccb2571e4e564f95af4
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-23 02:16:10 +0530

    complete course description page - where display the individual course description

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
+
 
 export const AppContext = createContext();
 
@@ -28,10 +30,39 @@ export const AppContextProvider = (props)=>{
         return totalRating / course.courseRatings.length
     }
 
+    // function to course chapter time
+
+    const calculateChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+    }
+
+    // function to calculate course duration
+
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture)=> time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+    }
+
+    //function to calculate number of lecture in the course
+
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length;
+            }
+        });
+        return totalLectures;
+
+    }
+
     useEffect(()=>{ fetchAllCourses()
     }, [])
-
-
 
     const value = {
         currency ,
@@ -39,8 +70,12 @@ export const AppContextProvider = (props)=>{
         navigate,
         calculateRating,
         isEducator,
-        setIsEducator
+        setIsEducator,
+        calculateChapterTime,
+        calculateCourseDuration,
+        calculateNoOfLectures
     }
+
     return (
         <AppContext.Provider value={value}>
             {props.children}
