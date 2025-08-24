0eb2940931090c46d477e08ae8086fece6aeacec
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-24 23:19:44 +0530

    start server side and install all dependency and connect to the mongodb

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+//intialize express
+const app = express()
+
+// connect to database
+await connectDB()
+
+
+//Middleware
+
+app.use(cors())
+
+// Routes
+
+app.get('/', (req, res)=> res.send("API Working"))
+
+//port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`)
+})
