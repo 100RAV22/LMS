f3b5946221e780cd52be654aea46b8d9d6ec0801
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-25 23:13:20 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
 
 //intialize express
 const app = express()
@@ -9,14 +10,12 @@ const app = express()
 // connect to database
 await connectDB()
 
-
 //Middleware
-
 app.use(cors())
 
 // Routes
-
 app.get('/', (req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
