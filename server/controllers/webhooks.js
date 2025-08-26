710a49554340308217dbb95454383e94fd7ffc11
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-26 16:00:26 +0530

    correct user

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,20 +1,22 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
+
 //API Controller function to manage clerk user with database
 
-export const clerkWebhooks = async (req, res ) => {
+export const clerkWebhooks = async (req, res) => {
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
-        })
-        const {data, type} = req.body
+        });
 
-        switch(type) {
+        const {data, type} = req.body;
+
+        switch (type) {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
@@ -38,7 +40,7 @@ export const clerkWebhooks = async (req, res ) => {
                 break;
             }
 
-            case 'user.deleted' : {
+            case 'user.deleted': {
                 await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
@@ -47,7 +49,7 @@ export const clerkWebhooks = async (req, res ) => {
             default:
                 break;
         }
-    }catch(error) {
-        res.json({success: false, message: error.message})
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
