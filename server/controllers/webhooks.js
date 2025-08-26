c1a777549c11c070aae5daad164c37af906c257f
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-26 11:51:01 +0530

    correct changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -20,7 +20,7 @@ export const clerkWebhooks = async (req, res ) => {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageurl: data.image_url
+                    imageurl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
